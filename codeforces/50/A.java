4695326387db0aaaf286b355422956f1073b2b3e
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2019-09-10 21:22:00 +0530

    Add solution for problem `A - Domino piling` Link: https://codeforces.com/contest/50/problem/A Tags: greedy, math, *800 Ref: https://codeforces.com/contest/50/submission/60355190

---
+++ codeforces/50/A.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Domino_piling {
+
+	public static void main(String[] args) {
+		Scanner t = new Scanner(System.in);
+
+		int a = t.nextInt();
+		int b = t.nextInt();
+
+		System.out.println((a * b) / 2);
+
+	}
+
+}
