da4d27a4de7b0e9df2a6fe8ba66c6d3241ced66c
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2020-03-11 20:00:00 +0530

    Add solution for problem `A - Petya and Strings` Link: https://codeforces.com/contest/112/problem/A Tags: implementation, strings, *800 Ref: https://codeforces.com/contest/112/submission/72961864

---
+++ codeforces/112/A.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Petya_And_Strings {
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FastReader t = new FastReader();
+		PrintWriter syso = new PrintWriter(System.out);
+
+		String s1 = t.next().toLowerCase();
+		String s2 = t.next().toLowerCase();
+
+		if (s1.compareTo(s2) < 0)
+			syso.println(-1);
+		else if (s1.compareTo(s2) > 0)
+			syso.println(1);
+		else
+			syso.println(0);
+
+		syso.close();
+
+	}
+
+}
