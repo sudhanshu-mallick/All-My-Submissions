990ff812be03157bdd3de07d43445b6b17874342
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2020-03-11 19:52:00 +0530

    Add solution for problem `A - Bit++` Link: https://codeforces.com/contest/282/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/282/submission/72961288

---
+++ codeforces/282/A.java
@@ -0,0 +1,75 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Bit {
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FastReader t = new FastReader();
+		PrintWriter o = new PrintWriter(System.out);
+		int test = t.nextInt();
+		int x = 0;
+
+		while (test-- > 0) {
+			StringBuilder s = new StringBuilder(t.next());
+			if (s.charAt(0) == '-') {
+				--x;
+			} else if (s.charAt(0) == '+') {
+				++x;
+			} else if (s.charAt(2) == '-') {
+				x--;
+			} else {
+				x++;
+			}
+		}
+
+		o.print(x);
+		o.close();
+	}
+
+}
