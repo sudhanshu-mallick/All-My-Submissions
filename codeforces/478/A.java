f74429c4c8ef984650df7d66fd1355e599ad8264
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2020-03-12 07:35:00 +0530

    Add solution for problem `A - Initial Bet` Link: https://codeforces.com/contest/478/problem/A Tags: implementation, *1100 Ref: https://codeforces.com/contest/478/submission/72992280

---
+++ codeforces/478/A.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Initial_Bet {
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FastReader t = new FastReader();
+		PrintWriter o = new PrintWriter(System.out);
+		int sum = 0;
+
+		for (int i = 0; i < 5; i++) {
+			sum += t.nextInt();
+		}
+
+		if (sum % 5 == 0 && sum != 0)
+			o.println(sum / 5);
+		else
+			o.println(-1);
+
+		o.flush();
+		o.close();
+	}
+
+}
