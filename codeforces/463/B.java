42b6ef04ceecdea70497d8161e699961b83f9821
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2020-03-14 10:32:00 +0530

    Add solution for problem `B - Caisa and Pylons` Link: https://codeforces.com/contest/463/problem/B Tags: brute force, implementation, math, *1100 Ref: https://codeforces.com/contest/463/submission/73186149

--- codeforces/463/B.java
+++ codeforces/463/B.java
@@ -1,7 +1,53 @@
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Caisa_And_Pylon {
+
+//	static class FastReader {
+//		BufferedReader br;
+//		StringTokenizer st;
+//
+//		public FastReader() {
+//			br = new BufferedReader(new InputStreamReader(System.in));
+//		}
+//
+//		String next() {
+//			while (st == null || !st.hasMoreElements()) {
+//				try {
+//					st = new StringTokenizer(br.readLine());
+//				} catch (IOException e) {
+//					e.printStackTrace();
+//				}
+//			}
+//			return st.nextToken();
+//		}
+//
+//		int nextInt() {
+//			return Integer.parseInt(next());
+//		}
+//
+//		long nextLong() {
+//			return Long.parseLong(next());
+//		}
+//
+//		double nextDouble() {
+//			return Double.parseDouble(next());
+//		}
+//
+//		String nextLine() {
+//			String str = "";
+//			try {
+//				str = br.readLine();
+//			} catch (IOException e) {
+//				e.printStackTrace();
+//			}
+//			return str;
+//		}
+//	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
