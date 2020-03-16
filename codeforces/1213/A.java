2df86a9622945c12f4692a78ed90ba71d85eed7d
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2020-03-16 19:42:00 +0530

    Add solution for problem `A - Chips Moving` Link: https://codeforces.com/contest/1213/problem/A Tags: math, *900 Ref: https://codeforces.com/contest/1213/submission/73414450

---
+++ codeforces/1213/A.java
@@ -0,0 +1,75 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Chips_Moving {
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			return str;
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FastReader t = new FastReader();
+		PrintWriter o = new PrintWriter(System.out);
+		int n = t.nextInt();
+		int even = 0, odd = 0;
+
+		for (int i = 0; i < n; i++) {
+			int a = t.nextInt();
+
+			if ((a & 1) == 1)
+				odd++;
+			else
+				even++;
+		}
+
+		o.println(Math.min(odd, even));
+		o.flush();
+		o.close();
+	}
+
+}
