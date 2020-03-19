e8acf55826b21f48581da06c118ff2bf2fea8237
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2020-03-19 13:42:00 +0530

    Add solution for problem `A - Creating a Character` Link: https://codeforces.com/contest/1217/problem/A Tags: binary search, math, *1300 Ref: https://codeforces.com/contest/1217/submission/73634777

---
+++ codeforces/1217/A.java
@@ -0,0 +1,79 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Creating_Character {
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FastReader t = new FastReader();
+		PrintWriter o = new PrintWriter(System.out);
+		int test = t.nextInt();
+
+		while (test-- > 0) {
+			int s = t.nextInt(), i = t.nextInt(), e = t.nextInt();
+			int p = (e - (s - i));
+			int ans = 0;
+
+			if (p < 0) {
+				ans = e + 1;
+			} else if (p == 0)
+				ans = e;
+			else if (p / 2 >= e)
+				ans = 0;
+			else
+				ans = e - p / 2;
+
+			o.println(ans);
+		}
+
+		o.flush();
+		o.close();
+	}
+
+}
