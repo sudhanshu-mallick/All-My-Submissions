be2ebf41a391fce998c769e046d7932c53d8f951
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2020-03-23 20:14:00 +0530

    Add solution for problem `A - Sum of Odd Integers` Link: https://codeforces.com/contest/1327/problem/A Tags: math, *1100 Ref: https://codeforces.com/contest/1327/submission/74057021

---
+++ codeforces/1327/A.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.util.*;
+
+public class Sum_Of_Odd_Integers {
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FastReader t = new FastReader();
+		PrintWriter o = new PrintWriter(System.out);
+		int test = t.nextInt();
+
+		while (test-- > 0) {
+			long n = t.nextLong();
+			long k = t.nextLong();
+
+			if ((n - k) % 2 == 0) {
+				if (k * k <= n)
+					o.println("YES");
+				else
+					o.println("NO");
+			} else
+				o.println("NO");
+		}
+
+		o.flush();
+		o.close();
+	}
+
+}
