0384eaaebf4a0c45c1723aedc0eecc846ff7ff4f
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2020-04-08 11:24:00 +0530

    Add solution for problem `B - New Year and the Treasure Geolocation` Link: https://codeforces.com/contest/1091/problem/B Tags: brute force, constructive algorithms, greedy, implementation, *1200 Ref: https://codeforces.com/contest/1091/submission/75805292

--- codeforces/1091/B.java
+++ codeforces/1091/B.java
@@ -51,17 +51,69 @@ public class Treasure_Geolocation {
 		FastReader t = new FastReader();
 		PrintWriter o = new PrintWriter(System.out);
 		int n = t.nextInt();
-		long sumx = 0, sumy = 0;
+		p1 a1[] = new p1[n];
+		p1 a2[] = new p1[n];
+		Compare1 o1 = new Compare1();
+		Compare2 o2 = new Compare2();
 
-		for (int i = 0; i < 2 * n; ++i) {
+		for (int i = 0; i < n; ++i) {
 			int x = t.nextInt(), y = t.nextInt();
-			sumx += x;
-			sumy += y;
+			a1[i] = new p1(x, y);
 		}
 
-		o.print(sumx / n + " " + sumy / n);
+		for (int i = 0; i < n; ++i) {
+			int x = t.nextInt(), y = t.nextInt();
+			a2[i] = new p1(x, y);
+		}
+
+		o1.compare(a1, n);
+		o2.compare(a2, n);
+
+		int x = a1[0].x + a2[0].x;
+		int y = a1[0].y + a2[0].y;
+
+		o.print(x + " " + y);
 
 		o.flush();
 		o.close();
+	}
+}
+
+class p1 {
+	int x, y;
+
+	public p1(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
+
+class Compare1 {
+
+	static void compare(p1 arr[], int n) {
+		Arrays.sort(arr, new Comparator<p1>() {
+			@Override
+			public int compare(p1 a, p1 b) {
+				if (a.x != b.x)
+					return a.x - b.x;
+				else
+					return a.y - b.y;
+			}
+		});
+	}
+}
+
+class Compare2 {
+
+	static void compare(p1 arr[], int n) {
+		Arrays.sort(arr, new Comparator<p1>() {
+			@Override
+			public int compare(p1 a, p1 b) {
+				if (a.x != b.x)
+					return b.x - a.x;
+				else
+					return b.y - a.y;
+			}
+		});
 	}
 }
