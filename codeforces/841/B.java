0ff7e77e4fe98ec7832c9202d83c3bee0f64bca6
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2020-04-08 18:48:00 +0530

    Add solution for problem `B - Godsend` Link: https://codeforces.com/contest/841/problem/B Tags: games, math, *1100 Ref: https://codeforces.com/contest/841/submission/75837603

---
+++ codeforces/841/B.java
@@ -0,0 +1,76 @@
+import java.util.*;
+import java.io.*;
+
+public class GodSend {
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FastReader t = new FastReader();
+		PrintWriter o = new PrintWriter(System.out);
+		int n = t.nextInt();
+		int even = 0, odd = 0;
+		String s = "";
+
+		for (int i = 0; i < n; i++) {
+			int x = t.nextInt();
+
+			if ((x & 1) == 0)
+				even++;
+			else
+				odd++;
+		}
+
+		if (odd == 0)
+			s = "Second";
+		else
+			s = "First";
+
+		o.println(s);
+
+		o.flush();
+		o.close();
+	}
+}
