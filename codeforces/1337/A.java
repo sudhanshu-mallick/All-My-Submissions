f59ed19a2891e27ab3fff50ed4d2210030bddbc8
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2020-04-15 20:09:00 +0530

    Add solution for problem `A - Ichihime and Triangle` Link: https://codeforces.com/contest/1337/problem/A Tags: constructive algorithms, math, *800 Ref: https://codeforces.com/contest/1337/submission/76809906

---
+++ codeforces/1337/A.java
@@ -0,0 +1,67 @@
+import java.util.*;
+import java.io.*;
+
+public class Triangle {
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FastReader t = new FastReader();
+		PrintWriter o = new PrintWriter(System.out);
+		int test = t.nextInt();
+
+		while (test-- > 0) {
+			int x = t.nextInt();
+			int y = t.nextInt();
+			int z = t.nextInt();
+			int a = t.nextInt();
+
+			o.println(y + " " + z + " " + z);
+		}
+
+		o.flush();
+		o.close();
+	}
+}
