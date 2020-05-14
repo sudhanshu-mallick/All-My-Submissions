be4854b62f0f1c9d8338fd77bcd2036fef5b2c6e
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2020-05-14 18:23:00 +0530

    Add solution for problem `C - Vladik and fractions` Link: https://codeforces.com/contest/743/problem/C Tags: brute force, constructive algorithms, math, number theory, *1500 Ref: https://codeforces.com/contest/743/submission/80070699

---
+++ codeforces/743/C.java
@@ -0,0 +1,63 @@
+import java.util.*;
+import java.io.*;
+
+public class Vladik_And_Fractions {
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FastReader t = new FastReader();
+		PrintWriter o = new PrintWriter(System.out);
+		int n = t.nextInt();
+
+		if (n != 1) {
+			o.println(n + " " + (n + 1) + " " + n * (n + 1));
+		} else
+			o.println("-1");
+
+		o.flush();
+		o.close();
+	}
+}
