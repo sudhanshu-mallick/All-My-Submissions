0ab7c147667d3ad8c0de99e2ad6bfe400b5c5424
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2020-05-16 17:15:00 +0530

    Add solution for problem `B - Young Explorers` Link: https://codeforces.com/contest/1355/problem/B Tags: dp, greedy, sortings, *1200 Ref: https://codeforces.com/contest/1355/submission/80313513

---
+++ codeforces/1355/B.java
@@ -0,0 +1,81 @@
+import java.util.*;
+import java.io.*;
+
+public class Young_Exploreers {
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FastReader t = new FastReader();
+		PrintWriter o = new PrintWriter(System.out);
+		int test = t.nextInt();
+
+		while (test-- > 0) {
+			int n = t.nextInt();
+			int[] a = new int[n];
+
+			for (int i = 0; i < n; ++i)
+				a[i] = t.nextInt();
+
+			Arrays.sort(a);
+
+			int count = 0, cur = 1;
+
+			for (int i = 0; i < n; ++i) {
+				if (a[i] <= cur) {
+					count++;
+					cur = 1;
+				} else {
+					cur++;
+				}
+			}
+
+			o.println(count);
+		}
+
+		o.flush();
+		o.close();
+	}
+}
