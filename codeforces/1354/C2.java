f663934d5765abf753a7f04211d0d4b56f030aa5
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2020-05-21 08:25:00 +0530

    Add solution for problem `C2 - Not So Simple Polygon Embedding` Link: https://codeforces.com/contest/1354/problem/C2 Tags: binary search, brute force, geometry, math, *2000 Ref: https://codeforces.com/contest/1354/submission/80816215

---
+++ codeforces/1354/C2.java
@@ -0,0 +1,66 @@
+import java.util.*;
+import java.io.*;
+
+public class Not_So_Simple_Polygon_Embedding {
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FastReader t = new FastReader();
+		PrintWriter o = new PrintWriter(System.out);
+		int test = t.nextInt();
+
+		while (test-- > 0) {
+			double n = t.nextDouble();
+			double cos = Math.cos(Math.PI / (4 * n));
+			double sin = Math.sin(Math.PI / (2 * n));
+
+			o.println(cos / sin);
+		}
+
+		o.flush();
+		o.close();
+	}
+}
