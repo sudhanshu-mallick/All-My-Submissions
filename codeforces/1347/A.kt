d8f106062cc632699ba99adbc170d6cfb7d680a8
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2020-05-22 22:13:00 +0530

    Add solution for problem `A - A+B (Trial Problem)` Link: https://codeforces.com/contest/1347/problem/A Tags: *special problem, *800 Ref: https://codeforces.com/contest/1347/submission/80954175

---
+++ codeforces/1347/A.kt
@@ -0,0 +1,14 @@
+import java.util.*
+//import kotlin.math.*
+ 
+fun main() {
+    val t = Scanner(System.`in`)
+    var test = t.nextInt()
+    
+    while(test-- > 0){
+        var a = t.nextInt()
+        var b = t.nextInt()
+        
+        println(a + b)
+    }
+}
