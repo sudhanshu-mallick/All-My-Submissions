3380db5f826d7978a3326cead751dc4fcfaade45
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2020-05-28 20:15:00 +0530

    Add solution for problem `A - Berland Poker` Link: https://codeforces.com/contest/1359/problem/A Tags: brute force, greedy, math, *1000 Ref: https://codeforces.com/contest/1359/submission/81722238

---
+++ codeforces/1359/A.java
@@ -0,0 +1,72 @@
+import java.util.*;
+import java.io.*;
+
+public class Berland_Poker {
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FastReader t = new FastReader();
+		PrintWriter o = new PrintWriter(System.out);
+		int test = t.nextInt();
+
+		while (test-- > 0) {
+			int n = t.nextInt();
+			int m = t.nextInt();
+			int k = t.nextInt();
+
+			if (n / k >= m)
+				o.println(m);
+			else {
+				int cm = m - n / k;
+				int max = cm % (k - 1) == 0 ? cm / (k - 1) : cm / (k - 1) + 1;
+				o.println(n / k - max);
+			}
+		}
+
+		o.flush();
+		o.close();
+	}
+}
