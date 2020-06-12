cf97b52d880e87b4a9d8eaa445a2789a4229c8a9
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2020-06-12 11:25:00 +0530

    Add solution for problem `C - Polycarp Restores Permutation` Link: https://codeforces.com/contest/1141/problem/C Tags: math, *1500 Ref: https://codeforces.com/contest/1141/submission/83501596

--- codeforces/1141/C.java
+++ codeforces/1141/C.java
@@ -1,11 +1,53 @@
 import java.util.*;
 import java.io.*;
 
-public class sample {
+public class Polycarp_Restores_Permutation {
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner t = new Scanner(System.in);
+		FastReader t = new FastReader();
 		PrintWriter o = new PrintWriter(System.out);
 		int n = t.nextInt() - 1;
 		long[] a = new long[n + 1];
