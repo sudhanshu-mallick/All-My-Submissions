d2019112b3d9db184511f672f0c66d5fab0f6fc8
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2020-06-18 12:18:00 +0530

    Add solution for problem `C - Candies!` Link: https://codeforces.com/contest/1189/problem/C Tags: data structures, dp, implementation, math, *1400 Ref: https://codeforces.com/contest/1189/submission/84132081

--- codeforces/1189/C.java
+++ codeforces/1189/C.java
@@ -2,56 +2,11 @@ import java.util.*;
 import java.io.*;
 
 public class Candies {
-	static class FastReader {
-		BufferedReader br;
-		StringTokenizer st;
-
-		public FastReader() {
-			br = new BufferedReader(new InputStreamReader(System.in));
-		}
-
-		String next() {
-			while (st == null || !st.hasMoreElements()) {
-				try {
-					st = new StringTokenizer(br.readLine());
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-			return st.nextToken();
-		}
-
-		int nextInt() {
-			return Integer.parseInt(next());
-		}
-
-		long nextLong() {
-			return Long.parseLong(next());
-		}
-
-		double nextDouble() {
-			return Double.parseDouble(next());
-		}
-
-		String nextLine() {
-			String str = "";
-			try {
-				str = br.readLine();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			return str;
-		}
-
-	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		FastReader t = new FastReader();
-		PrintWriter o = new PrintWriter(System.out);
+		Scanner t = new Scanner(System.in);
 		int n = t.nextInt();
 		int[] dp = new int[n + 1];
-		StringBuilder sb = new StringBuilder();
 
 		for (int i = 0; i < n; ++i) {
 			int x = t.nextInt();
@@ -64,12 +19,8 @@ public class Candies {
 			int x = t.nextInt();
 			int y = t.nextInt();
 
-			sb.append((dp[y] - dp[x - 1]) / 10);
-			sb.append("\n");
+			System.out.println((dp[y] - dp[x - 1]) / 10);
 		}
-
-		o.println(sb);
-		o.flush();
-		o.close();
 	}
+
 }
