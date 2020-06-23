0c405d27118af99e9cc9bed67e73a3fa326c19fd
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2020-06-23 13:48:00 +0530

    Add solution for problem `F - Consecutive Subsequence` Link: https://codeforces.com/contest/977/problem/F Tags: dp, *1700 Ref: https://codeforces.com/contest/977/submission/84725396

---
+++ codeforces/977/F.java
@@ -0,0 +1,89 @@
+import java.util.*;
+import java.io.*;
+
+public class Consecutive_Subsequnece {
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FastReader t = new FastReader();
+		PrintWriter o = new PrintWriter(System.out);
+		int n = t.nextInt();
+		int[] a = new int[n];
+		HashMap<Integer, Integer> map = new HashMap<>();
+		StringBuilder sb = new StringBuilder();
+		int max = 0, last = 0;
+
+		for (int i = 0; i < n; ++i) {
+			a[i] = t.nextInt();
+			int val = !map.containsKey(a[i] - 1) ? 1 : map.get(a[i] - 1) + 1;
+
+			map.put(a[i], val);
+
+			if (val > max) {
+				last = a[i];
+				max = val;
+			}
+		}
+
+		ArrayList<Integer> ans = new ArrayList<>();
+
+		for (int i = n - 1; i >= 0; --i) {
+			if (a[i] == last) {
+				ans.add(i + 1);
+				last--;
+			}
+		}
+
+		sb.append(ans.size() + "\n");
+
+		for (int i = ans.size() - 1; i >= 0; --i)
+			sb.append(ans.get(i) + " ");
+		
+		o.println(sb);
+		o.flush();
+		o.close();
+	}
+}
