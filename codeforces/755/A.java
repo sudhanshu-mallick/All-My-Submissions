85637b0e43dfcbb8fc2b07407fe27c63723680d0
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2020-06-26 13:03:00 +0530

    Add solution for problem `A - PolandBall and Hypothesis` Link: https://codeforces.com/contest/755/problem/A Tags: brute force, graphs, math, number theory, *800 Ref: https://codeforces.com/contest/755/submission/85101209

---
+++ codeforces/755/A.java
@@ -0,0 +1,65 @@
+import java.util.*;
+import java.io.*;
+
+public class PolandBall_And_Hypothesis {
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FastReader t = new FastReader();
+		PrintWriter o = new PrintWriter(System.out);
+		int n = t.nextInt();
+
+		if (n > 2)
+			o.println(n - 2);
+		else if (n == 2)
+			o.println(4);
+		else
+			o.println(3);
+
+		o.flush();
+		o.close();
+	}
+}
