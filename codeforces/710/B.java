5a7d1c27b9319f6d52c12c7f3cf6f264ffa42aa1
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2020-06-27 11:52:00 +0530

    Add solution for problem `B - Optimal Point on a Line` Link: https://codeforces.com/contest/710/problem/B Tags: brute force, sortings, *1400 Ref: https://codeforces.com/contest/710/submission/85181944

---
+++ codeforces/710/B.java
@@ -0,0 +1,66 @@
+import java.util.*;
+import java.io.*;
+
+public class Optimal_Points_On_A_Line {
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FastReader t = new FastReader();
+		PrintWriter o = new PrintWriter(System.out);
+		int n = t.nextInt();
+		List<Integer> a = new ArrayList<>();
+
+		for (int i = 0; i < n; ++i)
+			a.add(t.nextInt());
+
+		Collections.sort(a);
+
+		o.println(a.get((n - 1) >> 1));
+
+		o.flush();
+		o.close();
+	}
+}
