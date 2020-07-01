f08bbb4dd9b687ddbea8f81b6022cde74b9701e3
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2020-07-01 19:07:00 +0530

    Add solution for problem `A - Magical Sticks` Link: https://codeforces.com/contest/1371/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1371/submission/85626313

---
+++ codeforces/1371/A.java
@@ -0,0 +1,64 @@
+import java.util.*;
+import java.io.*;
+
+public class Magical_Sticks {
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FastReader t = new FastReader();
+		PrintWriter o = new PrintWriter(System.out);
+		int test = t.nextInt();
+
+		while (test-- > 0) {
+			long n = t.nextLong();
+
+			o.println((n + 1) / 2);
+		}
+
+		o.flush();
+		o.close();
+	}
+}
