8bbf2f406e5e6635f990dc68609e7e92b10e269b
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2020-07-11 20:38:00 +0530

    Add solution for problem `A - Omkar and Completion` Link: https://codeforces.com/contest/1372/problem/A Tags: constructive algorithms, implementation, *800 Ref: https://codeforces.com/contest/1372/submission/86518505

---
+++ codeforces/1372/A.java
@@ -0,0 +1,67 @@
+import java.util.*;
+import java.io.*;
+
+public class Omkar_And_Completion {
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FastReader t = new FastReader();
+		PrintWriter o = new PrintWriter(System.out);
+		int test = t.nextInt();
+
+		while (test-- > 0) {
+			int n = t.nextInt();
+
+			while (n-- > 0)
+				o.print("1 ");
+
+			o.println();
+		}
+
+		o.flush();
+		o.close();
+	}
+}
