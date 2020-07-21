903f3efbc49d005296048d7063e9250eb47565f1
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2020-07-21 20:03:00 +0530

    Add solution for problem `A - Even Odds` Link: https://codeforces.com/contest/318/problem/A Tags: math, *900 Ref: https://codeforces.com/contest/318/submission/87523037

---
+++ codeforces/318/A.java
@@ -0,0 +1,67 @@
+import java.util.*;
+import java.io.*;
+
+public class sample {
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FastReader t = new FastReader();
+		PrintWriter o = new PrintWriter(System.out);
+		long n = t.nextLong();
+		long k = t.nextLong();
+
+		if (k <= (n + 1) / 2) {
+			o.println(2 * k - 1);
+		} else {
+			k -= (n + 1) / 2;
+
+			o.println(2 * k);
+		}
+
+		o.flush();
+		o.close();
+	}
+}
