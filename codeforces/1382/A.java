11375015a57cbdb588e6f190710432adfe07dbea
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2020-07-21 20:11:00 +0530

    Add solution for problem `A - Common Subsequence` Link: https://codeforces.com/contest/1382/problem/A Tags: brute force, *800 Ref: https://codeforces.com/contest/1382/submission/87527931

---
+++ codeforces/1382/A.java
@@ -0,0 +1,85 @@
+import java.util.*;
+import java.io.*;
+
+public class Common_Subsequence {
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FastReader t = new FastReader();
+		PrintWriter o = new PrintWriter(System.out);
+		int test = t.nextInt();
+
+		while (test-- > 0) {
+			int m = t.nextInt();
+			int n = t.nextInt();
+			Set<Integer> set = new HashSet<>();
+			boolean flag = false;
+			int val = 0;
+
+			for (int i = 0; i < m; ++i)
+				set.add(t.nextInt());
+
+			for (int i = 0; i < n; ++i) {
+				int x = t.nextInt();
+
+				if (set.contains(x)) {
+					flag = true;
+					val = x;
+				}
+			}
+
+			if (flag) {
+				o.println("YES");
+				o.println("1 " + val);
+			} else
+				o.println("NO");
+
+		}
+
+		o.flush();
+		o.close();
+	}
+}
