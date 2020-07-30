2c1c0e82b2f11a29ff84799a3cc05f25ea850f66
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2020-07-30 20:29:00 +0530

    Add solution for problem `B - Captain Flint and a Long Voyage` Link: https://codeforces.com/contest/1388/problem/B Tags: greedy, math, *1000 Ref: https://codeforces.com/contest/1388/submission/88474998

---
+++ codeforces/1388/B.java
@@ -0,0 +1,72 @@
+import java.util.*;
+import java.io.*;
+
+public class Captain_Flint_And_Long_Voyage {
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FastReader t = new FastReader();
+		PrintWriter o = new PrintWriter(System.out);
+		int test = t.nextInt();
+
+		while (test-- > 0) {
+			int n = t.nextInt();
+			int _8 = (n - 1) / 4 + 1;
+			int _9 = n - _8;
+
+			while (_9-- > 0)
+				o.print("9");
+
+			while (_8-- > 0)
+				o.print("8");
+
+			o.println();
+		}
+
+		o.flush();
+		o.close();
+	}
+}
