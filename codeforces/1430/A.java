374b7c93705e275816af2a62f190ca100acdfa10
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2020-10-11 14:54:00 +0530

    Add solution for problem `A - Number of Apartments` Link: https://codeforces.com/contest/1430/problem/A Tags: brute force, constructive algorithms, math, *900 Ref: https://codeforces.com/contest/1430/submission/95206370

--- codeforces/1430/A.java
+++ codeforces/1430/A.java
@@ -51,20 +51,38 @@ public class Number_Of_Apartments {
 		FastReader t = new FastReader();
 		PrintWriter o = new PrintWriter(System.out);
 		int test = t.nextInt();
+		int[][] dp = new int[3][1001];
+
+		for (int i = 5; i <= 1000; ++i) {
+			if (i % 3 == 0)
+				dp[0][i] = i / 3;
+			else if (i % 5 == 0)
+				dp[1][i] = i / 5;
+			else if (i % 7 == 0) {
+				dp[2][i] = i / 7;
+			} else {
+				int cur = i;
+				int five = 0;
+
+				while (cur % 3 != 0) {
+					cur -= 5;
+					five++;
+				}
+
+				dp[0][i] = cur / 3;
+				dp[1][i] = five;
+			}
+		}
 
 		while (test-- > 0) {
 			int n = t.nextInt();
 
 			if (n == 1 || n == 2 || n == 4)
 				o.println("-1");
-			else {
-				if (n % 3 == 0)
-					o.println(n / 3 + " 0 0");
-				else if (n % 3 == 1)
-					o.println((n - 7) / 3 + " 0 1");
-				else
-					o.println((n - 5) / 3 + " 1 0");
-			}
+			else if (n == 3)
+				o.println("1 0 0");
+			else
+				o.println(dp[0][n] + " " + dp[1][n] + " " + dp[2][n]);
 		}
 
 		o.flush();
