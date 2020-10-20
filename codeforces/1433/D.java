6e0c6c227ce4c4c96bbc7beb807495300eb2d499
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2020-10-20 21:21:00 +0530

    Add solution for problem `D - Districts Connection` Link: https://codeforces.com/contest/1433/problem/D Tags: constructive algorithms, dfs and similar, *1200 Ref: https://codeforces.com/contest/1433/submission/96144931

--- codeforces/1433/D.java
+++ codeforces/1433/D.java
@@ -46,6 +46,22 @@ public class Districts_Connections {
 
 	}
 
+	public static void shuffle(int[] a) {
+		Random r = new Random();
+
+		for (int i = 0; i <= a.length - 2; i++) {
+			int j = i + r.nextInt(a.length - i);
+
+			swap(a, i, j);
+		}
+	}
+
+	public static void swap(int[] a, int i, int j) {
+		int temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		FastReader t = new FastReader();
@@ -53,50 +69,54 @@ public class Districts_Connections {
 		int test = t.nextInt();
 
 		while (test-- > 0) {
+			Map<Integer, Integer> map = new HashMap<>();
 			int n = t.nextInt();
-			int[] a = new int[n];
-			boolean flag = false;
+			List<int[]> ans = new ArrayList<>();
+			int max = Integer.MIN_VALUE;
+			int X = 0;
 
-			for (int i = 0; i < n; ++i) {
-				a[i] = t.nextInt();
+			for (int i = 0; i < n; i++) {
+				int x = t.nextInt();
+
+				map.put(x, map.getOrDefault(x, 0) + 1);
+				ans.add(new int[] { x, i + 1 });
+
+				max = Math.max(max, map.get(x));
 			}
 
-			int rep = a[0];
+			if (max == n) {
+				o.println("NO");
+			} else {
+				Collections.sort(ans, (x, y) -> x[0] - y[0] == 0 ? x[1] - y[1] : x[0] - y[0]);
 
-			for (int i = 1; i < n; ++i) {
-				if (a[i] != rep) {
-					flag = true;
-
-					break;
-				}
-			}
-
-			if (flag) {
 				o.println("YES");
 
-				int cur = a[0];
-				int idx = -1;
-
-				for (int i = 1; i < n; ++i) {
-					if (a[i] != cur) {
-						o.println((i + 1) + " 1");
-
-						idx = i + 1;
+				for (int i = n - 1; i >= 0; i--) {
+					if (ans.get(i)[0] == ans.get(0)[0]) {
+						X = i;
+						break;
 					}
+
+					o.println(ans.get(0)[1] + " " + ans.get(i)[1]);
 				}
 
-				for (int i = 1; i < n; ++i) {
-					if (a[i] == cur) {
-						o.println((i + 1) + " " + idx);
-					}
+				for (int i = 1; i <= X; i++) {
+					o.println(ans.get(i)[1] + " " + ans.get(n - 1)[1]);
 				}
-
-			} else {
-				o.println("NO");
 			}
 		}
 
 		o.flush();
 		o.close();
+	}
+
+	static class Pair {
+		int val;
+		Stack<Integer> list;
+
+		Pair(int val, Stack<Integer> list) {
+			this.val = val;
+			this.list = list;
+		}
 	}
 }
