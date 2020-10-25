9a9478c56ee90ff20abae42b028bfceb073083b1
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2020-10-25 16:40:00 +0530

    Add solution for problem `A - Finding Sasuke` Link: https://codeforces.com/contest/1435/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1435/submission/96650597

---
+++ codeforces/1435/A.java
@@ -0,0 +1,91 @@
+import java.util.*;
+import java.io.*;
+
+public class Finding_Sasuke {
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+	}
+
+	public static void shuffle(int[] a) {
+		Random r = new Random();
+
+		for (int i = 0; i <= a.length - 2; i++) {
+			int j = i + r.nextInt(a.length - i);
+
+			swap(a, i, j);
+		}
+	}
+
+	public static void swap(int[] a, int i, int j) {
+		int temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FastReader t = new FastReader();
+		PrintWriter o = new PrintWriter(System.out);
+		int test = t.nextInt();
+
+		while (test-- > 0) {
+			int n = t.nextInt();
+			int[] a = new int[n];
+
+			for (int i = 0; i < n; ++i)
+				a[i] = t.nextInt();
+
+			for (int i = n - 1; i >= n / 2; --i)
+				o.print(a[i] + " ");
+
+			for (int i = n / 2 - 1; i >= 0; --i) {
+				o.print((-a[i]) + " ");
+			}
+
+			o.println();
+		}
+
+		o.flush();
+		o.close();
+	}
+}
