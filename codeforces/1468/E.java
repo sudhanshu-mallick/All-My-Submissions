24dc5ec402177892ea4705e536443fe995d0f5be
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2020-12-25 17:12:00 +0530

    Add solution for problem `E - Four Segments` Link: https://codeforces.com/contest/1468/problem/E Tags: greedy, *800 Ref: https://codeforces.com/contest/1468/submission/102302700

---
+++ codeforces/1468/E.java
@@ -0,0 +1,70 @@
+import java.util.*;
+import java.io.*;
+
+public class Four_Segments {
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FastReader t = new FastReader();
+		PrintWriter o = new PrintWriter(System.out);
+		int test = t.nextInt();
+
+		while (test-- > 0) {
+			int[] a = new int[4];
+			a[0] = t.nextInt();
+			a[1] = t.nextInt();
+			a[2] = t.nextInt();
+			a[3] = t.nextInt();
+
+			Arrays.sort(a);
+
+			o.println(a[0] * a[2]);
+		}
+
+		o.flush();
+		o.close();
+	}
+}
