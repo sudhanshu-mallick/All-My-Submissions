aaa524d827d61b270953f23669355328ce88fbcd
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2020-12-30 20:12:00 +0530

    Add solution for problem `A - Bovine Dilemma` Link: https://codeforces.com/contest/1466/problem/A Tags: brute force, geometry, math, *800 Ref: https://codeforces.com/contest/1466/submission/102779410

---
+++ codeforces/1466/A.java
@@ -0,0 +1,93 @@
+import java.util.*;
+import java.io.*;
+
+public class Bovine_Dilemma {
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+	}
+
+	public static void shuffle(int[] a) {
+		Random r = new Random();
+
+		for (int i = 0; i <= a.length - 2; i++) {
+			int j = i + r.nextInt(a.length - i);
+
+			swap(a, i, j);
+		}
+	}
+
+	public static void swap(int[] a, int i, int j) {
+		int temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FastReader t = new FastReader();
+		PrintWriter o = new PrintWriter(System.out);
+		int test = t.nextInt();
+
+		while (test-- > 0) {
+			int n = t.nextInt();
+			Set<Integer> set = new HashSet<>();
+			int[] a = new int[n];
+
+			for (int i = 0; i < n; ++i)
+				a[i] = t.nextInt();
+
+			for (int i = 0; i < n - 1; ++i) {
+				for (int j = i + 1; j < n; ++j) {
+					int area = Math.abs(a[j] - a[i]);
+
+					set.add(area);
+				}
+			}
+
+			o.println(set.size());
+		}
+
+		o.flush();
+		o.close();
+	}
+}
