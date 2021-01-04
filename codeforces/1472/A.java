c2fc760bb384a5c096d266b10d6d4a99153e68e7
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2021-01-04 20:09:00 +0530

    Add solution for problem `A - Cards for Friends` Link: https://codeforces.com/contest/1472/problem/A Tags: greedy, math, *800 Ref: https://codeforces.com/contest/1472/submission/103188392

---
+++ codeforces/1472/A.java
@@ -0,0 +1,93 @@
+import java.util.*;
+import java.io.*;
+
+public class Cards_For_Freinds {
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+	}
+
+	public static void shuffle(int[] a) {
+		Random r = new Random();
+
+		for (int i = 0; i <= a.length - 2; i++) {
+			int j = i + r.nextInt(a.length - i);
+
+			swap(a, i, j);
+		}
+	}
+
+	public static void swap(int[] a, int i, int j) {
+		int temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FastReader t = new FastReader();
+		PrintWriter o = new PrintWriter(System.out);
+		int test = t.nextInt();
+
+		while (test-- > 0) {
+			long x = t.nextLong();
+			long y = t.nextLong();
+			long n = t.nextLong();
+			long a = x, b = y;
+
+			while ((x & 1) == 0)
+				x = x >> 1;
+
+			while ((y & 1) == 0)
+				y = y >> 1;
+
+			if (((a * b) / (x * y)) >= n)
+				o.println("YES");
+			else
+				o.println("NO");
+
+		}
+
+		o.flush();
+		o.close();
+	}
+}
