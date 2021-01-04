2def98d591b5b55ed0d57c647d077bd785ce4690
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2021-01-04 20:20:00 +0530

    Add solution for problem `C - Long Jumps` Link: https://codeforces.com/contest/1472/problem/C Tags: dp, graphs, *1100 Ref: https://codeforces.com/contest/1472/submission/103206934

---
+++ codeforces/1472/C.java
@@ -0,0 +1,99 @@
+import java.util.*;
+import java.io.*;
+
+public class Long_Jumps {
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+	}
+
+	public static void shuffle(int[] a) {
+		Random r = new Random();
+
+		for (int i = 0; i <= a.length - 2; i++) {
+			int j = i + r.nextInt(a.length - i);
+
+			swap(a, i, j);
+		}
+	}
+
+	public static void swap(int[] a, int i, int j) {
+		int temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FastReader t = new FastReader();
+		PrintWriter o = new PrintWriter(System.out);
+		int test = t.nextInt();
+
+		while (test-- > 0) {
+			int n = t.nextInt();
+			long[] a = new long[n];
+			long[] dp = new long[n];
+			long max = 0;
+
+			for (int i = 0; i < n; ++i)
+				a[i] = t.nextLong();
+
+			dp[n - 1] = a[n - 1];
+
+			for (int i = n - 2; i >= 0; --i) {
+				if (a[i] + i < (long) n)
+					dp[i] = a[i] + dp[i + (int) a[i]];
+				else
+					dp[i] = a[i];
+			}
+
+			for (int i = 0; i < n; ++i)
+				max = Math.max(dp[i], max);
+
+			o.println(max);
+
+		}
+
+		o.flush();
+		o.close();
+	}
+}
