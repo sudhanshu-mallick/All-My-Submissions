21eb8dc0b6ce3e9f6480a99402ea64dc26c9d80e
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2021-01-05 13:35:00 +0530

    Add solution for problem `A - Oh Those Palindromes` Link: https://codeforces.com/contest/1063/problem/A Tags: constructive algorithms, strings, *1300 Ref: https://codeforces.com/contest/1063/submission/103356521

---
+++ codeforces/1063/A.java
@@ -0,0 +1,84 @@
+import java.util.*;
+import java.io.*;
+
+public class Oh_Those_Palindromes {
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+	}
+
+	public static void shuffle(int[] a) {
+		Random r = new Random();
+
+		for (int i = 0; i <= a.length - 2; i++) {
+			int j = i + r.nextInt(a.length - i);
+
+			swap(a, i, j);
+		}
+	}
+
+	public static void swap(int[] a, int i, int j) {
+		int temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FastReader t = new FastReader();
+		PrintWriter o = new PrintWriter(System.out);
+		int n = t.nextInt();
+		int[] ch = new int[26];
+		String s = t.next();
+
+		while (n-- > 0) {
+			++ch[s.charAt(n) - 'a'];
+		}
+
+		for (int i = 0; i < 26; ++i)
+			while (ch[i]-- > 0)
+				o.print((char) (i + 'a'));
+
+		o.flush();
+		o.close();
+	}
+}
