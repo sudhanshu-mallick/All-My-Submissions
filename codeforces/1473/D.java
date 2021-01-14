2ba7d8b5aa21d494cd746c046a60ca02083a33c1
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2021-01-14 21:47:00 +0530

    Add solution for problem `D - Program` Link: https://codeforces.com/contest/1473/problem/D Tags: data structures, dp, implementation, strings, *1700 Ref: https://codeforces.com/contest/1473/submission/104337438

--- codeforces/1473/D.java
+++ codeforces/1473/D.java
@@ -46,6 +46,22 @@ public class Program {
 
 	}
 
+	public static void shuffle(int[] a) {
+		Random r = new Random();
+
+		for (int i = 0; i <= a.length - 2; i++) {
+			int j = i + r.nextInt(a.length - i);
+
+			swap(a, i, j);
+		}
+	}
+
+	public static void swap(int[] a, int i, int j) {
+		int temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		FastReader t = new FastReader();
@@ -66,34 +82,142 @@ public class Program {
 			}
 
 			a[n + 1] = a[n];
-			int[] preMin = new int[n + 2];
-			int[] preMax = new int[n + 2];
-			int[] sufMin = new int[n + 2];
-			int[] sufMax = new int[n + 2];
-			preMin[0] = preMax[0] = 0;
-			sufMin[n + 1] = sufMax[n + 1] = a[n + 1];
+			Min min = new Min();
+			Max max = new Max();
 
-			for (int i = 1; i <= n + 1; ++i) {
-				preMin[i] = Math.min(preMin[i - 1], a[i]);
-				preMax[i] = Math.max(preMax[i - 1], a[i]);
-				sufMin[n - i + 1] = Math.min(sufMin[n - i + 2], a[n - i + 1]);
-				sufMax[n - i + 1] = Math.max(sufMax[n - i + 2], a[n - i + 1]);
-			}
+			min.constructST(a, n + 2);
+			max.constructST(a, n + 2);
 
 			while (queries-- > 0) {
 				int left = t.nextInt();
 				int right = t.nextInt();
+				int mi = 0, ma = 0;
+				int MIN = min.RMQ(n + 2, 0, left - 1);
+				mi = Math.min(mi, MIN);
+				ma = Math.max(ma, MIN);
+				int MAX = max.RMQ(n + 2, 0, left - 1);
+				mi = Math.min(mi, MAX);
+				ma = Math.max(ma, MAX);
 				int offset = a[left - 1] - a[right];
-				int min, max;
-				min = Math.min(0, Math.min(preMin[left - 1], sufMin[right + 1] + offset));
-				max = Math.max(0, Math.max(preMax[left - 1], sufMax[right + 1] + offset));
+				MIN = min.RMQ(n + 2, right, n + 1);
+				mi = Math.min(mi, MIN + offset);
+				ma = Math.max(ma, MIN + offset);
+				MAX = max.RMQ(n + 2, right, n + 1);
+				mi = Math.min(mi, MAX + offset);
+				ma = Math.max(ma, MAX + offset);
 
-				o.println(max - min + 1);
+				o.println(ma - mi + 1);
 			}
 
 		}
 
 		o.flush();
 		o.close();
+	}
+
+	static class Min {
+		int st[];
+
+		int minVal(int x, int y) {
+			return (x < y) ? x : y;
+		}
+
+		int getMid(int s, int e) {
+			return s + (e - s) / 2;
+		}
+
+		int RMQUtil(int ss, int se, int qs, int qe, int index) {
+			if (qs <= ss && qe >= se)
+				return st[index];
+
+			if (se < qs || ss > qe)
+				return Integer.MAX_VALUE;
+
+			int mid = getMid(ss, se);
+
+			return minVal(RMQUtil(ss, mid, qs, qe, 2 * index + 1), RMQUtil(mid + 1, se, qs, qe, 2 * index + 2));
+		}
+
+		int RMQ(int n, int qs, int qe) {
+			if (qs < 0 || qe > n - 1 || qs > qe) {
+				System.out.println("Invalid Input");
+				return -1;
+			}
+
+			return RMQUtil(0, n - 1, qs, qe, 0);
+		}
+
+		int constructSTUtil(int arr[], int ss, int se, int si) {
+			if (ss == se) {
+				st[si] = arr[ss];
+				return arr[ss];
+			}
+
+			int mid = getMid(ss, se);
+			st[si] = minVal(constructSTUtil(arr, ss, mid, si * 2 + 1), constructSTUtil(arr, mid + 1, se, si * 2 + 2));
+
+			return st[si];
+		}
+
+		void constructST(int arr[], int n) {
+			int x = (int) (Math.ceil(Math.log(n) / Math.log(2)));
+			int max_size = 2 * (int) Math.pow(2, x) - 1;
+			st = new int[max_size];
+
+			constructSTUtil(arr, 0, n - 1, 0);
+		}
+	}
+
+	static class Max {
+		int st[];
+
+		int minVal(int x, int y) {
+			return (x < y) ? y : x;
+		}
+
+		int getMid(int s, int e) {
+			return s + (e - s) / 2;
+		}
+
+		int RMQUtil(int ss, int se, int qs, int qe, int index) {
+			if (qs <= ss && qe >= se)
+				return st[index];
+
+			if (se < qs || ss > qe)
+				return Integer.MIN_VALUE;
+
+			int mid = getMid(ss, se);
+
+			return minVal(RMQUtil(ss, mid, qs, qe, 2 * index + 1), RMQUtil(mid + 1, se, qs, qe, 2 * index + 2));
+		}
+
+		int RMQ(int n, int qs, int qe) {
+			if (qs < 0 || qe > n - 1 || qs > qe) {
+				System.out.println("Invalid Input");
+				return -1;
+			}
+
+			return RMQUtil(0, n - 1, qs, qe, 0);
+		}
+
+		int constructSTUtil(int arr[], int ss, int se, int si) {
+			if (ss == se) {
+				st[si] = arr[ss];
+				return arr[ss];
+			}
+
+			int mid = getMid(ss, se);
+			st[si] = minVal(constructSTUtil(arr, ss, mid, si * 2 + 1), constructSTUtil(arr, mid + 1, se, si * 2 + 2));
+
+			return st[si];
+		}
+
+		void constructST(int arr[], int n) {
+			int x = (int) (Math.ceil(Math.log(n) / Math.log(2)));
+			int max_size = 2 * (int) Math.pow(2, x) - 1;
+			st = new int[max_size];
+
+			constructSTUtil(arr, 0, n - 1, 0);
+		}
 	}
 }
