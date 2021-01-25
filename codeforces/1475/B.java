19b677d90055260b2d6cc126d455c761576e571d
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2021-01-25 23:12:00 +0530

    Add solution for problem `B - New Year's Number` Link: https://codeforces.com/contest/1475/problem/B Tags: brute force, dp, math, *900 Ref: https://codeforces.com/contest/1475/submission/105406269

---
+++ codeforces/1475/B.java
@@ -0,0 +1,85 @@
+import java.util.*;
+import java.io.*;
+
+public class New_Years_Number {
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+	}
+
+	public static void shuffle(int[] a) {
+		Random r = new Random();
+
+		for (int i = 0; i <= a.length - 2; i++) {
+			int j = i + r.nextInt(a.length - i);
+
+			swap(a, i, j);
+		}
+	}
+
+	public static void swap(int[] a, int i, int j) {
+		int temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FastReader t = new FastReader();
+		PrintWriter o = new PrintWriter(System.out);
+		int test = t.nextInt();
+
+		while (test-- > 0) {
+			int n = t.nextInt();
+			int _2021 = n % 2020;
+			int _2020 = (n - _2021) / 2020 - _2021;
+
+			if (_2020 >= 0 && _2021 * 2021 + _2020 * 2020 == n)
+				o.println("YES");
+			else
+				o.println("NO");
+		}
+
+		o.flush();
+		o.close();
+	}
+}
