ed78def50aa6f16fb7b1c938f0da2985f6eb14f1
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2021-01-28 20:07:00 +0530

    Add solution for problem `A - Nezzar and Colorful Balls` Link: https://codeforces.com/contest/1478/problem/A Tags: brute force, greedy, *800 Ref: https://codeforces.com/contest/1478/submission/105677346

---
+++ codeforces/1478/A.java
@@ -0,0 +1,103 @@
+import java.util.*;
+import java.io.*;
+
+public class Nezzar_And_Colorful_Balls {
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+	}
+
+	public static void shuffle(int[] a) {
+		Random r = new Random();
+
+		for (int i = 0; i <= a.length - 2; i++) {
+			int j = i + r.nextInt(a.length - i);
+
+			swap(a, i, j);
+		}
+	}
+
+	public static void swap(int[] a, int i, int j) {
+		int temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FastReader t = new FastReader();
+		PrintWriter o = new PrintWriter(System.out);
+		int test = t.nextInt();
+
+		while (test-- > 0) {
+			int max = 1;
+			int n = t.nextInt();
+			int[] a = new int[n];
+
+			for (int i = 0; i < n; ++i)
+				a[i] = t.nextInt();
+
+			int cur = a[0];
+			int cnt = 1;
+			int i = 1;
+
+			while (i < n) {
+				if (a[i] == cur) {
+					++cnt;
+				} else {
+					max = Math.max(cnt, max);
+					cnt = 1;
+					cur = a[i];
+				}
+
+				++i;
+			}
+
+			max = Math.max(cnt, max);
+
+			o.println(max);
+		}
+
+		o.flush();
+		o.close();
+	}
+}
