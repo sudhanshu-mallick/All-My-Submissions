48a00f053a9b154e71f8d1a87dff340ace1f1868
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2021-02-07 20:08:00 +0530

    Add solution for problem `A - Yet Another String Game` Link: https://codeforces.com/contest/1480/problem/A Tags: games, greedy, strings, *800 Ref: https://codeforces.com/contest/1480/submission/106751653

---
+++ codeforces/1480/A.java
@@ -0,0 +1,90 @@
+import java.util.*;
+import java.io.*;
+
+public class Yet_Another_String_Game {
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+	}
+
+	public static void shuffle(int[] a) {
+		Random r = new Random();
+
+		for (int i = 0; i <= a.length - 2; i++) {
+			int j = i + r.nextInt(a.length - i);
+
+			swap(a, i, j);
+		}
+
+		Arrays.sort(a);
+	}
+
+	public static void swap(int[] a, int i, int j) {
+		int temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FastReader t = new FastReader();
+		PrintWriter o = new PrintWriter(System.out);
+		int test = t.nextInt();
+
+		while (test-- > 0) {
+			char ch[] = t.next().toCharArray();
+
+			for (int i = 0; i < ch.length; ++i) {
+				if ((i & 1) == 0) {
+					ch[i] = ch[i] == 'a' ? 'b' : 'a';
+				} else {
+					ch[i] = ch[i] == 'z' ? 'y' : 'z';
+				}
+			}
+
+			o.println(new String(ch));
+		}
+
+		o.flush();
+		o.close();
+	}
+}
