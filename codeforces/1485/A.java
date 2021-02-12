dec3736a6930991eb3918afd052667fbd23ac43a
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2021-02-12 20:29:00 +0530

    Add solution for problem `A - Add and Divide` Link: https://codeforces.com/contest/1485/problem/A Tags: brute force, greedy, math, number theory, *1000 Ref: https://codeforces.com/contest/1485/submission/107194519

---
+++ codeforces/1485/A.java
@@ -0,0 +1,109 @@
+import java.util.*;
+import java.io.*;
+
+public class Add_And_Divide {
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+	}
+
+	public static void shuffle(int[] a) {
+		Random r = new Random();
+
+		for (int i = 0; i <= a.length - 2; i++) {
+			int j = i + r.nextInt(a.length - i);
+
+			swap(a, i, j);
+		}
+
+		Arrays.sort(a);
+	}
+
+	public static void swap(int[] a, int i, int j) {
+		int temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FastReader t = new FastReader();
+		PrintWriter o = new PrintWriter(System.out);
+		int test = t.nextInt();
+
+		while (test-- > 0) {
+			long a = t.nextLong();
+			long b = t.nextLong();
+
+			if (b > a) {
+				o.println("1");
+			} else {
+				long min = Integer.MAX_VALUE;
+				long dist = 0;
+
+				if (b == 1) {
+					++dist;
+					++b;
+				}
+
+				while (dist <= 32) {
+					long cur = b;
+					long times = 1;
+
+					while (cur <= a) {
+						cur = cur * b;
+						++times;
+					}
+
+					min = Math.min(dist + times, min);
+					++dist;
+					++b;
+				}
+
+				o.println(min);
+			}
+		}
+
+		o.flush();
+		o.close();
+	}
+}
