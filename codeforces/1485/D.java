54ce6d4a30b29531a209bdc9956a25a3df0e48b0
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2021-02-13 14:05:00 +0530

    Add solution for problem `D - Multiples and Power Differences` Link: https://codeforces.com/contest/1485/problem/D Tags: constructive algorithms, graphs, math, number theory, *2200 Ref: https://codeforces.com/contest/1485/submission/107265909

---
+++ codeforces/1485/D.java
@@ -0,0 +1,90 @@
+import java.util.*;
+import java.io.*;
+
+public class Multiples_And_Power_Differences {
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+	}
+
+	public static void shuffle(int[] a) {
+		Random r = new Random();
+
+		for (int i = 0; i <= a.length - 2; i++) {
+			int j = i + r.nextInt(a.length - i);
+
+			swap(a, i, j);
+		}
+
+		Arrays.sort(a);
+	}
+
+	public static void swap(int[] a, int i, int j) {
+		int temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FastReader t = new FastReader();
+		PrintWriter o = new PrintWriter(System.out);
+		int m = t.nextInt();
+		int n = t.nextInt();
+
+		for (int i = 0; i < m; ++i) {
+			for (int j = 0; j < n; ++j) {
+				int x = t.nextInt();
+
+				if (((i ^ j) & 1) == 1)
+					o.print("720720 ");
+				else
+					o.print(720720 + x * x * x * x + " ");
+			}
+
+			o.println();
+		}
+
+		o.flush();
+		o.close();
+	}
+}
