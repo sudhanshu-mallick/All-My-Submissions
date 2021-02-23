3b3eb88600cd5cf13619c6a0b9e8d2a66aca6d4b
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2021-02-23 19:07:00 +0530

    Add solution for problem `A - Three swimmers` Link: https://codeforces.com/contest/1492/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1492/submission/108304581

---
+++ codeforces/1492/A.java
@@ -0,0 +1,85 @@
+import java.util.*;
+import java.io.*;
+
+public class Three_Swimmers {
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+	}
+
+	public static void shuffle(int[] a) {
+		Random r = new Random();
+
+		for (int i = 0; i <= a.length - 2; i++) {
+			int j = i + r.nextInt(a.length - i);
+
+			swap(a, i, j);
+		}
+
+		Arrays.sort(a);
+	}
+
+	public static void swap(int[] a, int i, int j) {
+		int temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FastReader t = new FastReader();
+		PrintWriter o = new PrintWriter(System.out);
+		int test = t.nextInt();
+
+		while (test-- > 0) {
+			long p = t.nextLong();
+			long a = t.nextLong();
+			long b = t.nextLong();
+			long c = t.nextLong();
+
+			o.println(Math.min((p + a - 1) / a * a, Math.min((p + b - 1) / b * b, (p + c - 1) / c * c)) - p);
+		}
+
+		o.flush();
+		o.close();
+	}
+}
