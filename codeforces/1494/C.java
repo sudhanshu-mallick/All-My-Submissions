14d6638f02e2e3babdf1b0edf2d7a77fe0824557
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2021-03-04 01:22:00 +0530

    Add solution for problem `C - 1D Sokoban` Link: https://codeforces.com/contest/1494/problem/C Tags: binary search, dp, greedy, implementation, two pointers, *1900 Ref: https://codeforces.com/contest/1494/submission/109038057

--- codeforces/1494/C.java
+++ codeforces/1494/C.java
@@ -1,151 +1,118 @@
 import java.util.*;
 import java.io.*;
 
-public class _1D_Sokoban {
+public class C1494 {
 
-	static class FastReader {
-		BufferedReader br;
-		StringTokenizer st;
-
-		public FastReader() {
-			br = new BufferedReader(new InputStreamReader(System.in));
-		}
-
-		String next() {
-			while (st == null || !st.hasMoreElements()) {
-				try {
-					st = new StringTokenizer(br.readLine());
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
+	public static int solve(ArrayList<Integer> box, ArrayList<Integer> special) {
+		int i = 0;
+		int j = 0;
+		int ans = 0;
+		while (i < box.size() && j < special.size()) {
+			if (box.get(i).equals(special.get(j))) {
+				ans++;
+				i++;
+				j++;
+			} else if (box.get(i) < special.get(j)) {
+				i++;
+			} else {
+				j++;
 			}
-			return st.nextToken();
 		}
-
-		int nextInt() {
-			return Integer.parseInt(next());
-		}
-
-		long nextLong() {
-			return Long.parseLong(next());
-		}
-
-		double nextDouble() {
-			return Double.parseDouble(next());
-		}
-
-		String nextLine() {
-			String str = "";
-			try {
-				str = br.readLine();
-			} catch (IOException e) {
-				e.printStackTrace();
+		int max = ans;
+		i = 0;
+		for (j = 0; j < special.size(); j++) {
+			while (i < box.size() && box.get(i) <= special.get(j)) {
+				if (box.get(i).equals(special.get(j)))
+					ans--;
+				i++;
 			}
-			return str;
+			int bs = Collections.binarySearch(special, special.get(j) - i + 1);
+			if (bs < 0) {
+				bs = -bs - 1;
+			}
+			max = Math.max(max, ans + j - bs + 1);
 		}
-
+		return max;
 	}
 
-	public static void shuffle(int[] a) {
-		Random r = new Random();
-
-		for (int i = 0; i <= a.length - 2; i++) {
-			int j = i + r.nextInt(a.length - i);
-
-			swap(a, i, j);
-		}
-
-		Arrays.sort(a);
-	}
-
-	public static void swap(int[] a, int i, int j) {
-		int temp = a[i];
-		a[i] = a[j];
-		a[j] = temp;
-	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		FastReader t = new FastReader();
-		PrintWriter o = new PrintWriter(System.out);
-		int test = t.nextInt();
-
-		while (test-- > 0) {
-			int n = t.nextInt();
-			int m = t.nextInt();
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		PrintWriter pw = new PrintWriter(System.out);
+		int t = sc.nextInt();
+		while (t-- > 0) {
+			int n = sc.nextInt();
+			int m = sc.nextInt();
 			Stack<Integer> negBox = new Stack<Integer>();
 			Stack<Integer> negSpec = new Stack<Integer>();
 			ArrayList<Integer> posBox = new ArrayList<Integer>();
 			ArrayList<Integer> posSpec = new ArrayList<Integer>();
-
 			for (int i = 0; i < n; i++) {
-				int x = t.nextInt();
-
+				int x = sc.nextInt();
 				if (x > 0) {
 					posBox.add(x);
 				} else {
 					negBox.add(-x);
 				}
 			}
-
 			for (int i = 0; i < m; i++) {
-				int x = t.nextInt();
+				int x = sc.nextInt();
 				if (x > 0) {
 					posSpec.add(x);
 				} else {
 					negSpec.add(-x);
 				}
 			}
-
 			ArrayList<Integer> negBoxal = new ArrayList<Integer>();
-
 			while (!negBox.isEmpty()) {
 				negBoxal.add(negBox.pop());
 			}
-
 			ArrayList<Integer> negSpecal = new ArrayList<Integer>();
-
 			while (!negSpec.isEmpty()) {
 				negSpecal.add(negSpec.pop());
 			}
-
-			o.println(solve(posBox, posSpec) + solve(negBoxal, negSpecal));
+			pw.println(solve(posBox, posSpec) + solve(negBoxal, negSpecal));
 		}
-
-		o.flush();
-		o.close();
+		pw.close();
 	}
 
-	private static int solve(List<Integer> pos, List<Integer> spec) {
-		int n = pos.size();
-		int m = spec.size();
-		int[] suf = new int[n + 1];
-		int r = m - 1;
+	static class Scanner {
+		BufferedReader br;
+		StringTokenizer st;
 
-		for (int i = n - 1; i >= 0; --i) {
-			suf[i] = suf[i + 1];
-
-			while (r >= 0 && spec.get(r).intValue() > pos.get(i).intValue())
-				--r;
-
-			if (r >= 0 && spec.get(r).intValue() == pos.get(i).intValue())
-				++suf[i];
+		public Scanner(InputStream s) {
+			br = new BufferedReader(new InputStreamReader(s));
 		}
 
-		int ans = 0;
-		int j = 0;
-		r = 0;
-
-		for (int l = 0; l < m; ++l) {
-			while (j < n && pos.get(j).intValue() <= spec.get(l).intValue() + j)
-				++j;
-
-			while (r < m && spec.get(r).intValue() - spec.get(l).intValue() < j)
-				++r;
-
-			ans = Math.max(ans, r - l + suf[j]);
+		public Scanner(FileReader f) {
+			br = new BufferedReader(f);
 		}
 
-		return ans;
+		public String next() throws IOException {
+			while (st == null || !st.hasMoreTokens())
+				st = new StringTokenizer(br.readLine());
+			return st.nextToken();
+		}
+
+		public int nextInt() throws IOException {
+			return Integer.parseInt(next());
+		}
+
+		public long nextLong() throws IOException {
+			return Long.parseLong(next());
+		}
+
+		public double nextDouble() throws IOException {
+			return Double.parseDouble(next());
+		}
+
+		public int[] nextIntArr(int n) throws IOException {
+			int[] arr = new int[n];
+			for (int i = 0; i < n; i++) {
+				arr[i] = Integer.parseInt(next());
+			}
+			return arr;
+		}
+
 	}
+
 }
