18170edf46be5e38198876e6fa263b3631bc2adc
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2021-03-04 01:25:00 +0530

    Add solution for problem `C - 1D Sokoban` Link: https://codeforces.com/contest/1494/problem/C Tags: binary search, dp, greedy, implementation, two pointers, *1900 Ref: https://codeforces.com/contest/1494/submission/109038199

--- codeforces/1494/C.java
+++ codeforces/1494/C.java
@@ -74,136 +74,101 @@ public class _1D_Sokoban {
 			int m = t.nextInt();
 			int n = t.nextInt();
 			List<Integer> pos = new ArrayList<>();
-			List<Integer> specialPos = new ArrayList<>();
 			List<Integer> neg = new ArrayList<>();
-			List<Integer> specialNeg = new ArrayList<>();
+			List<Integer> posSpec = new ArrayList<>();
+			List<Integer> negSpec = new ArrayList<>();
 
 			for (int i = 0; i < m; ++i) {
-				long x = t.nextLong();
+				int x = t.nextInt();
 
 				if (x > 0)
-					pos.add((int) x);
+					pos.add(x);
 				else
-					neg.add((int) x);
+					neg.add(-x);
 			}
 
 			for (int i = 0; i < n; ++i) {
-				long x = t.nextLong();
+				int x = t.nextInt();
 
 				if (x > 0)
-					specialPos.add((int) x);
+					posSpec.add(x);
 				else
-					specialNeg.add((int) x);
+					negSpec.add(-x);
 			}
 
-			int count[] = new int[pos.size()];
-			int i = 0, j = 0;
-
-			while (i < pos.size()) {
-				if (j >= specialPos.size())
-					count[i] = i > 0 ? count[i - 1] : 0;
-				else {
-					while (j < specialPos.size() && pos.get(i).intValue() > specialPos.get(j).intValue())
-						++j;
-
-					if (j < specialPos.size() && specialPos.get(j).intValue() == pos.get(i).intValue())
-						count[i] = 1 + (i > 0 ? count[i - 1] : 0);
-					else
-						count[i] = i > 0 ? count[i - 1] : 0;
-				}
-
-				++i;
-			}
-
-			i = 0;
-			j = 0;
-
-			while (pos.size() > 0 && j < specialPos.size() && specialPos.get(j).intValue() < pos.get(0).intValue()) {
-				++j;
-			}
-
-			int idx = 0;
-			int jdx = 0;
-			int right = pos.size() > 0 ? count[pos.size() - 1] : 0;
-
-			while (i < pos.size() && j < specialPos.size()) {
-				int first = specialPos.get(j).intValue();
-
-				while (idx < pos.size() && idx + first >= pos.get(idx).intValue())
-					++idx;
-
-				--idx;
-
-				while (jdx < specialPos.size() && specialPos.get(jdx).intValue() <= idx + first)
-					++jdx;
-
-				--jdx;
-
-				right = Math.max(right, jdx - j + 1 + count[pos.size() - 1] - count[idx]);
-				++j;
-			}
-
-			for (i = 0; i < neg.size(); ++i)
-				neg.set(i, -neg.get(i));
-
-			for (i = 0; i < specialNeg.size(); ++i)
-				specialNeg.set(i, -specialNeg.get(i));
-
+			Collections.reverse(negSpec);
 			Collections.reverse(neg);
-			Collections.reverse(specialNeg);
 
-			count = new int[neg.size()];
-			i = 0;
-			j = 0;
+			int left = solve(neg, negSpec);
+			int right = solve(pos, posSpec);
 
-			while (i < neg.size()) {
-				if (j >= specialNeg.size())
-					count[i] = i > 0 ? count[i - 1] : 0;
-				else {
-					while (j < specialNeg.size() && neg.get(i).intValue() > specialNeg.get(j).intValue())
-						++j;
-
-					if (j < specialNeg.size() && specialNeg.get(j).intValue() == neg.get(i).intValue())
-						count[i] = 1 + (i > 0 ? count[i - 1] : 0);
-					else
-						count[i] = i > 0 ? count[i - 1] : 0;
-				}
-
-				++i;
-			}
-
-			i = 0;
-			j = 0;
-
-			while (neg.size() > 0 && j < specialNeg.size() && specialNeg.get(j).intValue() < neg.get(0).intValue()) {
-				++j;
-			}
-
-			idx = 0;
-			jdx = 0;
-			long left = neg.size() > 0 ? count[neg.size() - 1] : 0;
-
-			while (i < neg.size() && j < specialNeg.size()) {
-				int first = specialNeg.get(j).intValue();
-
-				while (idx < neg.size() && idx + first >= neg.get(idx).intValue())
-					++idx;
-
-				--idx;
-
-				while (jdx < specialNeg.size() && specialNeg.get(jdx).intValue() <= idx + first)
-					++jdx;
-
-				--jdx;
-
-				left = Math.max(left, jdx - j + 1 + count[neg.size() - 1] - count[idx]);
-				++j;
-			}
+//			System.out.println(left + " " + right);
 
 			o.println(left + right);
 		}
 
 		o.flush();
 		o.close();
+	}
+
+	private static int solve(List<Integer> pos, List<Integer> spec) {
+		if (pos.isEmpty() || spec.isEmpty() || spec.get(spec.size() - 1).intValue() < pos.get(0).intValue())
+			return 0;
+
+		int j = 0;
+		int[] dp = new int[pos.size() + 1];
+		int x = pos.size() - 1;
+		int y = spec.size() - 1;
+
+		while (x >= 0) {
+			if (y < 0) {
+				dp[x] = dp[x + 1];
+				--x;
+			} else if (pos.get(x).intValue() == spec.get(y).intValue()) {
+				--y;
+				dp[x] = dp[x + 1] + 1;
+				--x;
+			} else if (pos.get(x).intValue() > spec.get(y).intValue()) {
+				dp[x] = dp[x + 1];
+				--x;
+			} else
+				--y;
+		}
+
+		while (spec.get(j).intValue() < pos.get(0).intValue())
+			++j;
+
+		int max = dp[0];
+		int idx = 0;
+
+		while (j < spec.size()) {
+			x = 0;
+			y = pos.size() - 1;
+			int right = y;
+
+			while (x <= y) {
+				int mid = (x + y) >> 1;
+
+				if (pos.get(mid).intValue() > spec.get(j).intValue() + mid) {
+					y = mid - 1;
+				} else {
+					x = mid + 1;
+					right = mid;
+				}
+			}
+
+			while (idx < spec.size() && spec.get(idx).intValue() <= spec.get(j).intValue() + right) {
+				++idx;
+			}
+
+//			System.out.println(right + " " + idx + " " + j);
+
+			max = Math.max(max, idx - j + dp[right + 1]);
+			++j;
+		}
+
+//		System.out.println("Done");
+
+		return max;
 	}
 }
