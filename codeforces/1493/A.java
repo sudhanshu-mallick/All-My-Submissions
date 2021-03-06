d0d1ae9c8241412ed438e01cd950ab346bf5d8e6
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2021-03-06 19:43:00 +0530

    Add solution for problem `A - Anti-knapsack` Link: https://codeforces.com/contest/1493/problem/A Tags: constructive algorithms, greedy, *800 Ref: https://codeforces.com/contest/1493/submission/109231076

---
+++ codeforces/1493/A.java
@@ -0,0 +1,97 @@
+import java.util.*;
+import java.io.*;
+
+public class Anti_Knapsack {
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+	}
+
+	public static void shuffle(int[] a) {
+		Random r = new Random();
+
+		for (int i = 0; i <= a.length - 2; i++) {
+			int j = i + r.nextInt(a.length - i);
+
+			swap(a, i, j);
+		}
+
+		Arrays.sort(a);
+	}
+
+	public static void swap(int[] a, int i, int j) {
+		int temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FastReader t = new FastReader();
+		PrintWriter o = new PrintWriter(System.out);
+		int test = t.nextInt();
+
+		while (test-- > 0) {
+			int n = t.nextInt();
+			int k = t.nextInt();
+			List<Integer> ans = new ArrayList<>();
+
+			for (int i = k + 1; i <= n; ++i)
+				ans.add(i);
+
+			int i = k - 1;
+
+			while (i >= (k + 1) / 2)
+				ans.add(i--);
+
+			o.println(ans.size());
+
+			for (int v : ans)
+				o.print(v + " ");
+
+			o.println();
+		}
+
+		o.flush();
+		o.close();
+	}
+}
