fc6664365b58682422addd99163d8707b98000d8
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2021-03-25 17:53:00 +0530

    Add solution for problem `C - Ilya and Matrix` Link: https://codeforces.com/contest/313/problem/C Tags: constructive algorithms, greedy, implementation, sortings, *1400 Ref: https://codeforces.com/contest/313/submission/110974937

---
+++ codeforces/313/C.java
@@ -0,0 +1,91 @@
+import java.util.*;
+import java.io.*;
+
+public class Ilya_And_Matrix {
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+	}
+
+	public static void shuffle(int[] a) {
+		Random r = new Random();
+
+		for (int i = 0; i <= a.length - 2; i++) {
+			int j = i + r.nextInt(a.length - i);
+
+			swap(a, i, j);
+		}
+
+		Arrays.sort(a);
+	}
+
+	public static void swap(int[] a, int i, int j) {
+		int temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FastReader t = new FastReader();
+		PrintWriter o = new PrintWriter(System.out);
+		int n = t.nextInt();
+		List<Long> list = new ArrayList<>();
+		long ans = 0;
+
+		for (int i = 0; i < n; ++i)
+			list.add(t.nextLong());
+
+		Collections.sort(list, Collections.reverseOrder());
+
+		for (int i = 1; i < n; ++i)
+			list.set(i, list.get(i) + list.get(i - 1));
+
+		for (int i = 1; i <= n; i = i << 2) {
+			ans += list.get(i - 1);
+		}
+
+		o.println(ans);
+		o.flush();
+		o.close();
+	}
+}
