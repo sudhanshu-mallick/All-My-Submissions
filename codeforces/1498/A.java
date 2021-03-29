1c140530ecdc47f74ecaf8f9bd4b44f79eaf301f
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2021-03-29 20:09:00 +0530

    Add solution for problem `A - GCD Sum` Link: https://codeforces.com/contest/1498/problem/A Tags: brute force, math, *800 Ref: https://codeforces.com/contest/1498/submission/111358067

---
+++ codeforces/1498/A.java
@@ -0,0 +1,104 @@
+import java.util.*;
+import java.io.*;
+
+public class GCD_Sum {
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+	}
+
+	public static void shuffle(int[] a) {
+		Random r = new Random();
+
+		for (int i = 0; i <= a.length - 2; i++) {
+			int j = i + r.nextInt(a.length - i);
+
+			swap(a, i, j);
+		}
+
+		Arrays.sort(a);
+	}
+
+	public static void swap(int[] a, int i, int j) {
+		int temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FastReader t = new FastReader();
+		PrintWriter o = new PrintWriter(System.out);
+		int test = t.nextInt();
+
+		while (test-- > 0) {
+			long n = t.nextLong();
+
+			while (gcd(n, sumOfn(n)) == 1l)
+				++n;
+
+			o.println(n);
+		}
+
+		o.flush();
+		o.close();
+	}
+
+	private static long sumOfn(long n) {
+		long sum = 0;
+
+		while (n > 0) {
+			sum += n % 10;
+			n /= 10;
+		}
+
+		return sum;
+	}
+
+	private static long gcd(long a, long b) {
+		if (b == 0)
+			return a;
+
+		return gcd(b, a % b);
+	}
+
+}
