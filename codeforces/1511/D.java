2c79655f82c358414f60afe345459b36c298707f
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2021-04-13 00:09:00 +0530

    Add solution for problem `D - Min Cost String` Link: https://codeforces.com/contest/1511/problem/D Tags: brute force, constructive algorithms, graphs, greedy, strings, *1600 Ref: https://codeforces.com/contest/1511/submission/112869158

---
+++ codeforces/1511/D.java
@@ -0,0 +1,93 @@
+import java.util.*;
+import java.io.*;
+
+public class Min_Cost_String {
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+	}
+
+	public static void shuffle(int[] a) {
+		Random r = new Random();
+
+		for (int i = 0; i <= a.length - 2; i++) {
+			int j = i + r.nextInt(a.length - i);
+
+			swap(a, i, j);
+		}
+
+		Arrays.sort(a);
+	}
+
+	public static void swap(int[] a, int i, int j) {
+		int temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FastReader t = new FastReader();
+		PrintWriter o = new PrintWriter(System.out);
+		int n = t.nextInt();
+		int k = t.nextInt();
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < k; ++i) {
+			char x = (char) (i + 'a');
+
+			sb.append(x);
+
+			for (int j = i + 1; j < k; ++j) {
+				char y = (char) (j + 'a');
+
+				sb.append(x + "" + y);
+			}
+		}
+
+		for (int i = 0; i < n; ++i)
+			o.print(sb.charAt(i % sb.length()));
+
+		o.flush();
+		o.close();
+	}
+}
