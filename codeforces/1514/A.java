d0e0ef2224b3c69007d4f31f208815a6363ca502
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2021-04-19 19:09:00 +0530

    Add solution for problem `A - Perfectly Imperfect Array` Link: https://codeforces.com/contest/1514/problem/A Tags: math, number theory, *800 Ref: https://codeforces.com/contest/1514/submission/113493143

---
+++ codeforces/1514/A.java
@@ -0,0 +1,95 @@
+import java.util.*;
+import java.io.*;
+
+public class Perfectly_Imperfect_Array {
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+	}
+
+	public static void shuffle(int[] a) {
+		Random r = new Random();
+
+		for (int i = 0; i <= a.length - 2; i++) {
+			int j = i + r.nextInt(a.length - i);
+
+			swap(a, i, j);
+		}
+
+		Arrays.sort(a);
+	}
+
+	public static void swap(int[] a, int i, int j) {
+		int temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FastReader t = new FastReader();
+		PrintWriter o = new PrintWriter(System.out);
+		int test = t.nextInt();
+		Set<Integer> set = new HashSet<>();
+
+		for (int i = 1; i <= 100; ++i)
+			set.add(i * i);
+
+		while (test-- > 0) {
+			int n = t.nextInt();
+			boolean flag = false;
+
+			for (int i = 0; i < n; ++i) {
+				int x = t.nextInt();
+
+				if (!set.contains(x)) {
+					flag = true;
+				}
+			}
+
+			o.println(flag ? "YES" : "NO");
+		}
+
+		o.flush();
+		o.close();
+	}
+}
