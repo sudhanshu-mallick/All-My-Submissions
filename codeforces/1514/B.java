1a9255998a179ee970151369e7a59eeb01dfdb6e
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2021-04-19 19:17:00 +0530

    Add solution for problem `B - AND 0, Sum Big` Link: https://codeforces.com/contest/1514/problem/B Tags: bitmasks, combinatorics, math, *1200 Ref: https://codeforces.com/contest/1514/submission/113500774

---
+++ codeforces/1514/B.java
@@ -0,0 +1,88 @@
+import java.util.*;
+import java.io.*;
+
+public class And_0_Sum_Big {
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+	}
+
+	public static void shuffle(int[] a) {
+		Random r = new Random();
+
+		for (int i = 0; i <= a.length - 2; i++) {
+			int j = i + r.nextInt(a.length - i);
+
+			swap(a, i, j);
+		}
+
+		Arrays.sort(a);
+	}
+
+	public static void swap(int[] a, int i, int j) {
+		int temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FastReader t = new FastReader();
+		PrintWriter o = new PrintWriter(System.out);
+		int test = t.nextInt();
+
+		while (test-- > 0) {
+			long n = t.nextLong();
+			long mod = 1000000007;
+			long k = t.nextLong();
+			long cur = 1;
+
+			while (k-- > 0)
+				cur = (cur * n) % mod;
+
+			o.println(cur);
+		}
+
+		o.flush();
+		o.close();
+	}
+}
