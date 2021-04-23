58b4b53d39a6ed9eaebb693548f2304a4d0dbe1e
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2021-04-23 10:57:00 +0530

    Add solution for problem `A - Average Height` Link: https://codeforces.com/contest/1509/problem/A Tags: constructive algorithms, *800 Ref: https://codeforces.com/contest/1509/submission/113942420

---
+++ codeforces/1509/A.java
@@ -0,0 +1,99 @@
+import java.util.*;
+import java.io.*;
+
+public class Average_Height {
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+	}
+
+	public static void shuffle(int[] a) {
+		Random r = new Random();
+
+		for (int i = 0; i <= a.length - 2; i++) {
+			int j = i + r.nextInt(a.length - i);
+
+			swap(a, i, j);
+		}
+
+		Arrays.sort(a);
+	}
+
+	public static void swap(int[] a, int i, int j) {
+		int temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FastReader t = new FastReader();
+		PrintWriter o = new PrintWriter(System.out);
+		int test = t.nextInt();
+
+		while (test-- > 0) {
+			int n = t.nextInt();
+			List<Integer> odd = new ArrayList<>();
+			List<Integer> even = new ArrayList<>();
+
+			while (n-- > 0) {
+				int x = t.nextInt();
+
+				if ((x & 1) == 0)
+					even.add(x);
+				else
+					odd.add(x);
+			}
+
+			for (int v : even)
+				o.print(v + " ");
+
+			for (int v : odd)
+				o.print(v + " ");
+
+			o.println();
+		}
+
+		o.flush();
+		o.close();
+	}
+}
