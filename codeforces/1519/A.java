9fa2df9ad566a3d63931603630d1d4155105c051
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2021-04-30 13:37:00 +0530

    Add solution for problem `A - Red and Blue Beans` Link: https://codeforces.com/contest/1519/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1519/submission/114666208

---
+++ codeforces/1519/A.java
@@ -0,0 +1,84 @@
+import java.util.*;
+import java.io.*;
+
+public class Red_And_Blue_Jeans {
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+	}
+
+	public static void shuffle(int[] a) {
+		Random r = new Random();
+
+		for (int i = 0; i <= a.length - 2; i++) {
+			int j = i + r.nextInt(a.length - i);
+
+			swap(a, i, j);
+		}
+
+		Arrays.sort(a);
+	}
+
+	public static void swap(int[] a, int i, int j) {
+		int temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FastReader t = new FastReader();
+		PrintWriter o = new PrintWriter(System.out);
+		int test = t.nextInt();
+
+		while (test-- > 0) {
+			long r = t.nextLong();
+			long b = t.nextLong();
+			long d = t.nextLong();
+
+			o.println(Math.max(r, b) > (d + 1) * Math.min(r, b) ? "NO" : "YES");
+		}
+
+		o.flush();
+		o.close();
+	}
+}
