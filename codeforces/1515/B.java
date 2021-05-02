7e797f977574774a4baf9aa4147db8eb18f486e8
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2021-05-02 21:51:00 +0530

    Add solution for problem `B - Phoenix and Puzzle` Link: https://codeforces.com/contest/1515/problem/B Tags: brute force, geometry, math, number theory, *1000 Ref: https://codeforces.com/contest/1515/submission/114938083

---
+++ codeforces/1515/B.java
@@ -0,0 +1,90 @@
+import java.util.*;
+import java.io.*;
+
+public class Phoenix_And_Puzzle {
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+	}
+
+	public static void shuffle(int[] a) {
+		Random r = new Random();
+
+		for (int i = 0; i <= a.length - 2; i++) {
+			int j = i + r.nextInt(a.length - i);
+
+			swap(a, i, j);
+		}
+
+		Arrays.sort(a);
+	}
+
+	public static void swap(int[] a, int i, int j) {
+		int temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FastReader t = new FastReader();
+		PrintWriter o = new PrintWriter(System.out);
+		int test = t.nextInt();
+
+		while (test-- > 0) {
+			long n = t.nextLong();
+			long n1 = n / 2;
+			long n2 = n / 4;
+			long sq1 = (long) Math.sqrt(n1);
+			long sq2 = (long) Math.sqrt(n2);
+			boolean flag = (sq1 * sq1 == n1 && (n & 1) == 0) || (sq2 * sq2 == n2 && (n & 1) == 0 && (n1 & 1) == 0);
+
+			if (flag)
+				o.println("YES");
+			else
+				o.println("NO");
+		}
+
+		o.flush();
+		o.close();
+	}
+}
