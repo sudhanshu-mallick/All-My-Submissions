b43543e2f862bef181313aaf9f52d502aebd02b9
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2021-05-05 13:08:00 +0530

    Add solution for problem `B - Roadside Trees (Simplified Edition)` Link: https://codeforces.com/contest/265/problem/B Tags: greedy, implementation, *1000 Ref: https://codeforces.com/contest/265/submission/115180843

---
+++ codeforces/265/B.java
@@ -0,0 +1,87 @@
+import java.util.*;
+import java.io.*;
+
+public class Roadside_Trees {
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+	}
+
+	public static void shuffle(int[] a) {
+		Random r = new Random();
+
+		for (int i = 0; i <= a.length - 2; i++) {
+			int j = i + r.nextInt(a.length - i);
+
+			swap(a, i, j);
+		}
+
+		Arrays.sort(a);
+	}
+
+	public static void swap(int[] a, int i, int j) {
+		int temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FastReader t = new FastReader();
+		PrintWriter o = new PrintWriter(System.out);
+		int n = t.nextInt();
+		int prev = 0;
+		int ans = -1;
+
+		while (n-- > 0) {
+			int x = t.nextInt();
+			ans += Math.abs(prev - x);
+			prev = x;
+			++ans;
+			++ans;
+		}
+
+		o.println(ans);
+		o.flush();
+		o.close();
+	}
+}
