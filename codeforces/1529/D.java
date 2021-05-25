dd7712f015dbf0bb230072e38fcd1b46863875f8
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2021-05-25 19:03:00 +0530

    Add solution for problem `D - Kavi on Pairing Duty` Link: https://codeforces.com/contest/1529/problem/D Tags: combinatorics, dp, math, number theory, *1700 Ref: https://codeforces.com/contest/1529/submission/117327029

---
+++ codeforces/1529/D.java
@@ -0,0 +1,90 @@
+import java.util.*;
+import java.io.*;
+
+public class Kavi_On_Pairing_Duty {
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+	}
+
+	public static void shuffle(int[] a) {
+		Random r = new Random();
+
+		for (int i = 0; i <= a.length - 2; i++) {
+			int j = i + r.nextInt(a.length - i);
+
+			swap(a, i, j);
+		}
+
+		Arrays.sort(a);
+	}
+
+	public static void swap(int[] a, int i, int j) {
+		int temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FastReader t = new FastReader();
+		PrintWriter o = new PrintWriter(System.out);
+		int n = t.nextInt();
+		long[] dp = new long[n + 1];
+		long pre = 1;
+		long mod = 998244353;
+
+		for (int i = 1; i <= n; ++i)
+			for (int j = i; j <= n; j += i)
+				++dp[j];
+
+		for (int i = 2; i <= n; ++i) {
+			dp[i] = (dp[i] + pre) % mod;
+			pre = (pre + dp[i]) % mod;
+		}
+
+		o.println(dp[n]);
+
+		o.flush();
+		o.close();
+	}
+}
