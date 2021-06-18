03c55fa95616cb8dfaf79f32570e7ec48f139b2b
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2021-06-18 21:51:00 +0530

    Add solution for problem `D - Deleting Divisors` Link: https://codeforces.com/contest/1537/problem/D Tags: games, math, number theory, *1700 Ref: https://codeforces.com/contest/1537/submission/119897790

---
+++ codeforces/1537/D.java
@@ -0,0 +1,89 @@
+
+import java.util.*;
+import java.io.*;
+
+public class Deleting_Divisors {
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+	}
+
+	public static void shuffle(int[] a) {
+		Random r = new Random();
+
+		for (int i = 0; i <= a.length - 2; i++) {
+			int j = i + r.nextInt(a.length - i);
+
+			swap(a, i, j);
+		}
+
+		Arrays.sort(a);
+	}
+
+	public static void swap(int[] a, int i, int j) {
+		int temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FastReader t = new FastReader();
+		PrintWriter o = new PrintWriter(System.out);
+		int test = t.nextInt();
+
+		while (test-- > 0) {
+			int n = t.nextInt();
+			int two = 0;
+
+			while ((n & 1) == 0) {
+				++two;
+				n >>= 1;
+			}
+
+			o.println(two > 0 && (n != 1 || ((two & 1) == 0)) ? "Alice" : "Bob");
+		}
+
+		o.flush();
+		o.close();
+	}
+}
