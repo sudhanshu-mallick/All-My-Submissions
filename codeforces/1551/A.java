5d4234206587094d6ac3be483b8ead875875f3d2
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2021-07-23 20:08:00 +0530

    Add solution for problem `A - Polycarp and Coins` Link: https://codeforces.com/contest/1551/problem/A Tags: greedy, math, *800 Ref: https://codeforces.com/contest/1551/submission/123445987

---
+++ codeforces/1551/A.java
@@ -0,0 +1,94 @@
+import java.util.*;
+import java.io.*;
+
+public class Polycarp_And_Coins {
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+	}
+
+	public static void shuffle(int[] a) {
+		Random r = new Random();
+
+		for (int i = 0; i <= a.length - 2; i++) {
+			int j = i + r.nextInt(a.length - i);
+
+			swap(a, i, j);
+		}
+
+		Arrays.sort(a);
+	}
+
+	public static void swap(int[] a, int i, int j) {
+		int temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FastReader t = new FastReader();
+		PrintWriter o = new PrintWriter(System.out);
+		int test = t.nextInt();
+
+		while (test-- > 0) {
+			int n = t.nextInt();
+			int c1 = 0, c2 = 0;
+
+			if (n % 3 == 0) {
+				c1 = n / 3;
+				c2 = n / 3;
+			} else if (n % 3 == 1) {
+				c1 = n / 3 + 1;
+				c2 = n / 3;
+			} else {
+				c1 = n / 3;
+				c2 = n / 3 + 1;
+			}
+
+			o.println(c1 + " " + c2);
+		}
+
+		o.flush();
+		o.close();
+	}
+}
