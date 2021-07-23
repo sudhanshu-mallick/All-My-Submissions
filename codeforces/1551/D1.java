b643d7208463f5bd46a2aea0389c85307174b15f
Sudhanshu Mallick <sudhanshumallick9@gmail.com>
2021-07-23 21:17:00 +0530

    Add solution for problem `D1 - Domino (easy version)` Link: https://codeforces.com/contest/1551/problem/D1 Tags: constructive algorithms, math, *1700 Ref: https://codeforces.com/contest/1551/submission/123502579

---
+++ codeforces/1551/D1.java
@@ -0,0 +1,93 @@
+import java.util.*;
+import java.io.*;
+
+public class Domino {
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+	}
+
+	public static void shuffle(int[] a) {
+		Random r = new Random();
+
+		for (int i = 0; i <= a.length - 2; i++) {
+			int j = i + r.nextInt(a.length - i);
+
+			swap(a, i, j);
+		}
+
+		Arrays.sort(a);
+	}
+
+	public static void swap(int[] a, int i, int j) {
+		int temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FastReader t = new FastReader();
+		PrintWriter o = new PrintWriter(System.out);
+		int test = t.nextInt();
+
+		while (test-- > 0) {
+			int m = t.nextInt();
+			int n = t.nextInt();
+			int k = t.nextInt();
+			boolean flag = true;
+
+			if ((m & 1) == 1) {
+				n >>= 1;
+				flag = k >= n && (((k - n) & 1) == 0);
+			} else if ((n & 1) == 1) {
+				flag = (k & 1) == 0 && (k <= (m * n) / 2 - m / 2);
+			} else
+				flag = (k & 1) == 0;
+
+			o.println(flag ? "YES" : "NO");
+		}
+
+		o.flush();
+		o.close();
+	}
+}
